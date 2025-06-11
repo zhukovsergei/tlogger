094e574b47a4b4f3d2aec17e789d8fef57771a89
Zhukov Sergei <alivesystemsonly@gmail.com>
2025-06-11 20:07:59 +0200

    Fixed CORS issue

---
+++ app/backend/src/main/java/ch/tlogger/app/config/RestDataConfig.java
@@ -0,0 +1,20 @@
+package ch.tlogger.app.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class RestDataConfig implements RepositoryRestConfigurer {
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        cors.addMapping("/**")
+            .allowedOriginPatterns("*")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "PATCH")
+            .allowedHeaders("*")
+            .allowCredentials(false)
+            .maxAge(3600);
+    }
+} 
