4824c5e8ca43efa5b7e1fa9438b4128e8d607d9a
Zhukov Sergei <alivesystemsonly@gmail.com>
2025-06-19 01:36:00 +0200

    Fixed issues with style calendar

--- app/frontend/app/layout.tsx
+++ app/frontend/app/layout.tsx
@@ -1,8 +1,9 @@
 import '@mantine/core/styles.css';
+import '@mantine/dates/styles.css';
 
 import React from 'react';
 import { ColorSchemeScript, mantineHtmlProps, MantineProvider } from '@mantine/core';
-import { theme } from '../theme';
+import { theme } from '@/theme';
 
 export const metadata = {
   title: 'Mantine Next.js template',
