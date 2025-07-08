a03537c65c32849081d34700ba651559e54b02b2
Zhukov Sergei <alivesystemsonly@gmail.com>
2025-07-08 23:03:16 +0200

    Added users

---
+++ app/backend/src/main/java/ch/tlogger/app/domain/User.java
@@ -0,0 +1,32 @@
+package ch.tlogger.app.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.util.Set;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @Column(name = "id", updatable = false, nullable = false, columnDefinition = "uuid")
+    private UUID id;
+
+    @Column(name = "username", nullable = false, unique = true)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"))
+    @Column(name = "role")
+    private Set<String> roles;
+} 
---
+++ app/backend/src/main/java/ch/tlogger/app/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package ch.tlogger.app.repository;
+
+import ch.tlogger.app.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByUsername(String username);
+} 
