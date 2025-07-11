3b9f90771715797da998f32a73375c7947d45252
Zhukov Sergei <alivesystemsonly@gmail.com>
2025-07-11 02:26:29 +0200

    Added navbar

--- app/frontend/components/Navbar/Navbar.tsx
+++ app/frontend/components/Navbar/Navbar.tsx
@@ -5,11 +5,14 @@ export default function Navbar() {
   return (
     <Box component="nav" style={{ borderBottom: '1px solid #eee', marginBottom: 32 }}>
       <Container size="lg" style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', height: 60 }}>
-        <Anchor component={Link} href="/" size="lg" underline={false}>
+        <Anchor component={Link} href="/" size="lg" underline="never">
           Tlogger
         </Anchor>
         <Group>
-          <Anchor component={Link} href="/login" size="md">
+          <Anchor component={Link} href="/dashboard" size="md" underline="hover">
+            Dashboard
+          </Anchor>
+          <Anchor component={Link} href="/login" size="md" underline="hover">
             Login
           </Anchor>
         </Group>
